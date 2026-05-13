76e50153462f5927f9c2b1fb40215c9a8c2f1e99
pro <pro@192.168.1.51>
2026-05-13 23:26:12 +0800

    liste Produit

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,14 +1,59 @@
 import { Component } from '@angular/core';
 import { ListComponent } from '../product/components/list-component/list-component';
 import { NavbarComponent } from "../../shared/navbar-component/navbar-component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [ListComponent, NavbarComponent],
+  imports: [ListComponent, NavbarComponent,CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
 export class Home {
 
+  products = [
+    {
+      title: 'T-shirt Cool',
+      description: 'Un T-shirt qui claque, à porter partout.',
+      price: '29,99€',
+      tag: 'Nouveau',
+      image: 'assets/img/1.jpeg'
+    },
+    {
+      title: 'Casque Audio',
+      description: 'Son exceptionnel, confort au rendez-vous.',
+      price: '49,99€',
+      tag: 'Populaire',
+      image: 'assets/img/2.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/3.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/4.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/5.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/6.jpeg'
+    }
+    ];
 
 }
--- src/app/features/product/components/list-component/list-component.ts
+++ src/app/features/product/components/list-component/list-component.ts
@@ -15,21 +15,42 @@ products = [
       description: 'Un T-shirt qui claque, à porter partout.',
       price: '29,99€',
       tag: 'Nouveau',
-      image: 'assets/images/tshirt.jpg'
+      image: 'assets/img/1.jpeg'
     },
     {
       title: 'Casque Audio',
       description: 'Son exceptionnel, confort au rendez-vous.',
       price: '49,99€',
       tag: 'Populaire',
-      image: 'assets/images/headphones.jpg'
+      image: 'assets/img/2.jpeg'
     },
     {
       title: 'Mug Fun',
       description: 'Pour le café ou le thé, toujours stylé.',
       price: '19,99€',
       tag: 'Best-seller',
-      image: 'assets/images/mug.jpg'
+      image: 'assets/img/3.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/4.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/5.jpeg'
+    },
+    {
+      title: 'Mug Fun',
+      description: 'Pour le café ou le thé, toujours stylé.',
+      price: '19,99€',
+      tag: 'Best-seller',
+      image: 'assets/img/6.jpeg'
     }
     ];
 }
